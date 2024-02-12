b193193a6363e56ae782ab8ccac1ad282a7c7cf3
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-02-12 14:24:04 +0500

    updated

--- runner.php
+++ runner.php
@@ -3,17 +3,4 @@
 
 require_once './setup.php';
 
-GoogleAI::SetConfig(getConfig());
-
-$tasks = [
-    CheckInboxForReplies::class,
-    PostWorkPlan::class,
-    PostProjectIdea::class,
-    ReplyToBaseCampMessages::class,
-];
-
-foreach ($tasks as $task) {
-    $task::execute();
-
-    sleep(3);
-}
+logMessage('All is well!');
