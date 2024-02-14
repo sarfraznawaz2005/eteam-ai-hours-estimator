dd6c2a79dc99aeb5cd48527117176840da0614ff
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-02-14 10:00:31 +0500

    updated

--- classes/tasks/PostWorkPlan.php
+++ classes/tasks/PostWorkPlan.php
@@ -2,6 +2,8 @@
 
 class PostWorkPlan extends Task
 {
+    protected static $totalNewPostsToFetch = 3;
+
     public static function execute()
     {
         //logMessage('Running: ' . __CLASS__);
@@ -22,51 +24,54 @@ class PostWorkPlan extends Task
         $eteamMiscProjectMessages = BasecampClassicAPI::getAllMessages($eteamMiscTasksProjectId);
 
         if (is_array($eteamMiscProjectMessages) && $eteamMiscProjectMessages) {
-            $messsageId = key(array_slice($eteamMiscProjectMessages, 0, 1, true));
-            $messageValue = reset($eteamMiscProjectMessages);
+            
+            $messages = array_slice($eteamMiscProjectMessages, 0, static::$totalNewPostsToFetch, true);
 
-            if (
-                str_starts_with(strtolower(trim($messageValue)), 'workplan') ||
-                str_starts_with(strtolower(trim($messageValue)), 'work plan')
-            ) {
+            foreach ($messages as $messageId => $messageValue) {
 
-                $message = <<<message
-                AOA,<br><br>
+                if (
+                    str_starts_with(strtolower(trim($messageValue)), 'workplan') ||
+                    str_starts_with(strtolower(trim($messageValue)), 'work plan')
+                ) {
 
-                - Post Project Idea<br>
-                - Code Reviews<br>
-                - Email Communication<br>
-                - Basecamp Communication<br>
-                - etc
-                message;
+                    $message = <<<message
+                    AOA,<br><br>
 
-                GoogleAI::setPrompt("Please provide a inspirational quote tailored to our software engineering company. This inspirational quote should boost the morale of our team.");
+                    - Post Project Idea<br>
+                    - Code Reviews<br>
+                    - Email Communication<br>
+                    - Basecamp Communication<br>
+                    - etc
+                    message;
 
-                $response = GoogleAI::GenerateContentWithRetry();
+                    GoogleAI::setPrompt("Please provide a inspirational quote tailored to our software engineering company. This inspirational quote should boost the morale of our team.");
 
-                if (!str_contains(strtolower($response), 'no response')) {
-                    $message .= <<<message
+                    $response = GoogleAI::GenerateContentWithRetry();
+
+                    if (!str_contains(strtolower($response), 'no response')) {
+                        $message .= <<<message
                             <br><br><b>Inspirational Quote Of The Day:</b><br>
 
                             $response
                         message;
-                }
+                    }
 
-                $action = "posts/$messsageId/comments.xml";
+                    $action = "posts/$messageId/comments.xml";
 
-                $xmlData = <<<data
+                    $xmlData = <<<data
                 <comment>
                     <body><![CDATA[$message]]></body>
                 </comment>
                 data;
 
-                // send to basecamp
-                $response = BasecampClassicAPI::postInfo($action, $xmlData);
+                    // send to basecamp
+                    $response = BasecampClassicAPI::postInfo($action, $xmlData);
 
-                if ($response && $response['code'] === 201) {
-                    static::markDone(__CLASS__, __CLASS__);
-                } else {
-                    logMessage(__CLASS__ . " :  Could not post workplan", 'danger');
+                    if ($response && $response['code'] === 201) {
+                        static::markDone(__CLASS__, __CLASS__);
+                    } else {
+                        logMessage(__CLASS__ . " :  Could not post workplan", 'danger');
+                    }
                 }
             }
         }
