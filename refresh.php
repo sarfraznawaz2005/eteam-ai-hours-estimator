436533716432eb1f880ef821085eedca66dd6501
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-02-18 17:03:46 +0500

    updated

--- refresh.php
+++ refresh.php
@@ -1,4 +1,40 @@
 <?php
+/////////////////////////////////////
+// delete tasks lock files
+/////////////////////////////////////
+
+// Array of prefixes for lock files to skip (e.g., 'composer', 'package', etc.)
+$skipLockFiles = ['composer', 'package'];
 
 $files = glob('*.lock');
-print_r($files);
+
+foreach ($files as $file) {
+
+    // Check if file name starts with one of the prefixes in $skipLockFiles
+    foreach ($skipLockFiles as $prefix) {
+        if (strpos($file, $prefix) === 0) {
+            echo "Skipping lock file: $file<br>";
+            continue;
+        }
+    }
+
+    // Try to open the file
+    $handle = @fopen($file, "r+");
+
+    // If the file could be opened
+    if ($handle) {
+        // Try to lock the file for exclusive access
+        if (flock($handle, LOCK_EX | LOCK_NB)) {
+            // If the lock was acquired, unlock and close handle, then unlink (delete) the file
+            flock($handle, LOCK_UN);
+            fclose($handle);
+
+            if (@unlink($file)) {
+                echo "Deleted lock file: $file<br>";
+            }
+        } else {
+            // Couldn't lock the file, it might be in use, so close the handle and skip deletion
+            fclose($handle);
+        }
+    }
+}
