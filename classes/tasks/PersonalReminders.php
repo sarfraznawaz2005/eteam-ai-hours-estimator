d54642705b234cd46706cec083b1a8d83e09125b
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-03-15 16:05:07 +0500

    updated

--- classes/tasks/PersonalReminders.php
+++ classes/tasks/PersonalReminders.php
@@ -18,23 +18,23 @@ class PersonalReminders extends Task
             return;
         }
 
-        self::chekcReminder('Sarfraz Net Bill Reminder', '15', 'sarfraz@eteamid.com', 'Dear Sarfraz,<br><br>Kindly pay internet bill today.');
-        self::chekcReminder('Sarfraz Net Bill Reminder', '14', 'sarfraz@eteamid.com', 'Dear Sarfraz,<br><br>Kindly pay internet bill today.');
-        self::chekcReminder('Sarfraz Net Bill Reminder', '16', 'sarfraz@eteamid.com', 'Dear Sarfraz,<br><br>Kindly pay internet bill today.');
+        self::checkReminder('Sarfraz Net Bill Reminder', '15', 'sarfraz@eteamid.com', 'Dear Sarfraz,<br><br>Kindly pay internet bill today.');
+        self::checkReminder('Sarfraz Net Bill Reminder', '14', 'sarfraz@eteamid.com', 'Dear Sarfraz,<br><br>Kindly pay internet bill today.');
+        self::checkReminder('Sarfraz Net Bill Reminder', '16', 'sarfraz@eteamid.com', 'Dear Sarfraz,<br><br>Kindly pay internet bill today.');
 
-        self::chekcReminder('Sarfraz Net Bill Reminder', date('Y-m-t', strtotime(date('Y-m-d'))), 'sarfraz@eteamid.com', 'Dear Sarfraz,<br><br>Kindly pay internet bill today.');
-        self::chekcReminder('K-Electric Bill Reminder', '15', 'sarfraz@eteamid.com', 'Dear Sarfraz,<br><br>Kindly pay K-Electric bills today.');
-        self::chekcReminder('Team Salaries Reminder', '4', 'riaz@eteamid.com', 'Dear Riaz,<br><br>This is your reminder for Team Salaries.');
-        self::chekcReminder('FBL Hoe Loan Instalment Reminder', '6', 'riaz@eteamid.com', 'Dear Riaz,<br><br>This is your reminder for FBL Hoe Loan Instalment.');
-        self::chekcReminder('Complete Invoicing Reminder', '7', 'riaz@eteamid.com', 'Dear Riaz,<br><br>This is your reminder for Complete Invoicing.');
-        self::chekcReminder('Office and Home Utilities Reminder', '8', 'riaz@eteamid.com', 'Dear Riaz,<br><br>This is your reminder for Office and Home Utilities.');
-        self::chekcReminder('Team Allowances Reminder', '12', 'riaz@eteamid.com', 'Dear Riaz,<br><br>This is your reminder for Team Allowances.');
-        self::chekcReminder('FBR & SRB Tax Challans Reminder', '13', 'riaz@eteamid.com', 'Dear Riaz,<br><br>This is your reminder for FBR & SRB Tax Challans.');
-        self::chekcReminder('CC Payments Reminder', '20', 'riaz@eteamid.com', 'Dear Riaz,<br><br>This is your reminder for CC Payments.');
+        self::checkReminder('Sarfraz Net Bill Reminder', date('Y-m-t', strtotime(date('Y-m-d'))), 'sarfraz@eteamid.com', 'Dear Sarfraz,<br><br>Kindly pay internet bill today.');
+        self::checkReminder('K-Electric Bill Reminder', '15', 'sarfraz@eteamid.com', 'Dear Sarfraz,<br><br>Kindly pay K-Electric bills today.');
+        self::checkReminder('Team Salaries Reminder', '4', 'riaz@eteamid.com', 'Dear Riaz,<br><br>This is your reminder for Team Salaries.');
+        self::checkReminder('FBL Hoe Loan Instalment Reminder', '6', 'riaz@eteamid.com', 'Dear Riaz,<br><br>This is your reminder for FBL Hoe Loan Instalment.');
+        self::checkReminder('Complete Invoicing Reminder', '7', 'riaz@eteamid.com', 'Dear Riaz,<br><br>This is your reminder for Complete Invoicing.');
+        self::checkReminder('Office and Home Utilities Reminder', '8', 'riaz@eteamid.com', 'Dear Riaz,<br><br>This is your reminder for Office and Home Utilities.');
+        self::checkReminder('Team Allowances Reminder', '12', 'riaz@eteamid.com', 'Dear Riaz,<br><br>This is your reminder for Team Allowances.');
+        self::checkReminder('FBR & SRB Tax Challans Reminder', '13', 'riaz@eteamid.com', 'Dear Riaz,<br><br>This is your reminder for FBR & SRB Tax Challans.');
+        self::checkReminder('CC Payments Reminder', '20', 'riaz@eteamid.com', 'Dear Riaz,<br><br>This is your reminder for CC Payments.');
 
     }
 
-    private static function chekcReminder($prefix, $day, $to, $body): void
+    private static function checkReminder($prefix, $day, $to, $body): void
     {
         $todayDate = date('Y-m-d');
 
