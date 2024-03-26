0b9910d4c1295f75476ac5b551245490f65e1d09
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-03-26 05:37:59 +0500

    updated

--- classes/tasks/CodingTipOfTheDay.php
+++ classes/tasks/CodingTipOfTheDay.php
@@ -33,7 +33,7 @@ class CodingTipOfTheDay extends Task
             return;
         }
 
-        GoogleAI::setPrompt("\n\nPlease generate a detailed and useful tip tailored to software engineering or web designing with example code if needed. It should be based on PHP, laravel, javascript, reactjs, devops, git, css, or some other web technology.");
+        GoogleAI::setPrompt("\n\nPlease generate a detailed and useful tip tailored to software engineering or web designing with example code if needed. It should be based on PHP, laravel, javascript, reactjs, devops, git, css, ai, project management, db, web development, mobile development, best practices, performance, security, tech tools, or some other web technology. Do provide reference links if possible.");
 
         $response = GoogleAI::GenerateContentWithRetry();
 
