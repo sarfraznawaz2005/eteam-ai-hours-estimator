e4fe5fc7e89e24c9c08efc19fa826467e70aa1ad
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2024-06-04 10:26:36 +0500

    updated

--- classes/tasks/PostProjectIdea.php
+++ classes/tasks/PostProjectIdea.php
@@ -41,7 +41,7 @@ class PostProjectIdea extends Task
             return;
         }
 
-        GoogleAI::setPrompt(file_get_contents(basePath() . '/tools/idea-generator/prompt.txt') . "\n\nPlease generate a random but realistic and unique web application or mobile product idea based on given instructions. Idea must not be trivial such as time tracker or todoist, etc and should be useful for a business or a consumer.");
+        GoogleAI::setPrompt(file_get_contents(basePath() . '/tools/idea-generator/prompt.txt') . "\n\nPlease generate a random but realistic and unique web application or mobile product idea based on given instructions. Idea must not be trivial such as time tracker or todoist, etc.");
 
         $response = GoogleAI::GenerateContentWithRetry();
 
